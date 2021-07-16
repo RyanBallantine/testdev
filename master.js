d43f55f60538d851461b4cda830df6d268045be2
Ryan Ballantine <rgballantine@gmail.com>
2021-07-16 20:50:35 +0200

    Initial commit

---
+++ master.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class master extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}

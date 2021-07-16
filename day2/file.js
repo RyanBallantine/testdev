d6d45b4f5a744c97a5ea39fd6245dcac3ed9796e
Ryan Ballantine <rgballantine@gmail.com>
2021-07-16 20:59:05 +0200

    Day2

---
+++ day2/file.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class file extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
